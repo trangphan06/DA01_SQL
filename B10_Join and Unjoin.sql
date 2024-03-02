8d587ab7b4854221d20fa1e326eb884b58164070
trangphan06 <160846048+trangphan06@users.noreply.github.com>
2024-03-02 22:54:45 +0700

    Update B10_Join and Unjoin.sql

--- B10_Join and Unjoin.sql
+++ B10_Join and Unjoin.sql
@@ -76,3 +76,86 @@ on a.seat_no=b.seat_no
 group by right(a.seat_no,1)
 order by count(*) DESC -- hạng A đc đặt thường xuyên nhất
 ;
+
+-- FULL JOIN
+--- tìm những vé máy bay không được cấp thẻ lên máy bay
+select count(*)
+from boarding_passes as a
+full join tickets as b
+on a.ticket_no=b.ticket_no
+where a.boarding_no is null
+;
+
+-- JOIN ON MULTIPLE CONDITIONS
+--- có những trường hợp phải cần 2 keys để thông tin được đúng nhất
+/* 
+Tính giá trung bình của từng số ghế máy bay
+- Xdinh output, input => số ghế, giá trung bình
+*/
+select a.seat_no, avg(b.amount) as average_amount
+from boarding_passes as a
+left join ticket_flights as b
+on a.ticket_no=b.ticket_no 
+and a.flight_id=b.flight_id
+group by a.seat_no
+order by avg(b.amount)
+;
+
+-- JOIN MULTIPLE TABLES
+--- truy vấn bảng gồm số vé, tên KH, giá vé, giờ bay, giờ kết thúc 
+select a.ticket_no, a.passenger_name, b.amount, c.scheduled_departure, c.scheduled_arrival
+from tickets as a
+inner join ticket_flights as b on a.ticket_no=b.ticket_no
+inner join flights as c on b.flight_id=c.flight_id
+;
+
+-- SELF JOIN
+CREATE TABLE employee (
+	employee_id INT,
+	name VARCHAR (50),
+	manager_id INT
+);
+
+INSERT INTO employee 
+VALUES
+	(1, 'Liam Smith', NULL),
+	(2, 'Oliver Brown', 1),
+	(3, 'Elijah Jones', 1),
+	(4, 'William Miller', 1),
+	(5, 'James Davis', 2),
+	(6, 'Olivia Hernandez', 2),
+	(7, 'Emma Lopez', 2),
+	(8, 'Sophia Andersen', 2),
+	(9, 'Mia Lee', 3),
+	(10, 'Ava Robinson', 3);
+
+select emp.employee_id, emp.name as emp_name, emp.manager_id, mng.name as mng_name
+from employee as emp 
+left join employee as mng 
+on emp.manager_id = mng.employee_id
+;
+
+-- UNION
+/*
+- Số lượng cột ở 2 bảng giống nhau
+- Kiểu dữ liệu trong cùng 1 cột giống nhau
+- UNION loại trùng lặp, UNION ALL thì không
+*/
+
+--- cú pháp
+select col1,col2,col3...
+from table1
+UNION/UNION ALL
+select col1,col2,...
+from table 2
+UNION/UNION ALL
+select col1,col2,...
+from table 3
+...
+
+select first_name, 'actor' as source from actor
+union all
+select first_name, 'customer' as source from customer
+union all
+select first_name, 'staff' as source from staff
+;
