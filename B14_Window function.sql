c1e52d1d4c38de3d38b4f22fc86474c80613838d
trangphan06 <160846048+trangphan06@users.noreply.github.com>
2024-03-13 23:32:38 +0700

    Update B14_Window function.sql

--- B14_Window function.sql
+++ B14_Window function.sql
@@ -1,4 +1,11 @@
 -- WINDOW FUNCTION with SUM(), COUNT(), AVG(), COUNT()
+--- OVER(PARTITION BY...)
+
+/* Phân biệt Window function vs. Group by
+- Group by: gộp lại các row giống nhau và chỉ trả ra 1 kqua đã gộp
+- Window function: không gộp lại, trả ra 1 kết quả như nhau theo từng khối/window
+*/
+
 /* Tính tỉ lệ số tiền thanh toán từng ngày với
 tổng số tiền đã thanh toán của mỗi KH
 Output: mã KH, tên KH, ngày thanh toán, số tiền thanh toán tại ngày, 
@@ -28,3 +35,34 @@ on a.customer_id=b.customer_id
 group by a.customer_id, to_char(b.payment_date, 'yyyy-mm-dd')
 order by a.customer_id, to_char(b.payment_date, 'yyyy-mm-dd')
 ;
+
+--- Challenge 1a:
+select a.film_id, a.title, a.length, c.name, 
+avg(length) over(partition by c.name) as avg_cgr_length
+from film as a
+join film_category as b
+on a.film_id=b.film_id 
+join category as c
+on b.category_id=c.category_id
+order by a.film_id
+;
+
+--- Challenge 1b:
+select *,
+count(*) over(partition by customer_id, amount) as count
+from payment
+order by payment_id
+;
+
+--- OVER(ORDER BY...)
+SELECT payment_date, amount,
+SUM(amount) OVER(ORDER BY payment_date) AS aggregate_total
+FROM PAYMENT
+;
+
+-- CÚ PHÁP
+SELECT col1, col2,
+AGG(col2) OVER(PARTITION BY col1,col2 ORDER BY col3) 
+FROM table
+
+
