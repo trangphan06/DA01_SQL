633910c4a8004c5428c7616e0631399f2efefa90
trangphan06 <160846048+trangphan06@users.noreply.github.com>
2024-04-11 23:16:46 +0700

    Update B19_Project1.sql

--- B19_Project1.sql
+++ B19_Project1.sql
@@ -61,4 +61,64 @@ SET MONTH_ID=EXTRACT(month FROM orderdate),
 	YEAR_ID=EXTRACT(year FROM orderdate),
 	QTR_ID=EXTRACT(quarter FROM orderdate);
 
+-- 5. Tìm outlier (nếu có) cho cột QUANTITYORDERED và hãy chọn cách xử lý cho bản ghi đó (2 cách) ( Không chạy câu lệnh trước khi bài được review)
+-- 5.1 Delete Outliers Using boxplot/IQR 
+with outliers as (
+with min_max_table as 
+(
+select 
+Q1-1.5*IQR as min_value,
+Q3+1.5*IQR as max_value
+from 
+(
+select 
+percentile_cont(0.25) within group (order by quantityordered) as Q1,
+percentile_cont(0.75) within group (order by quantityordered) as Q3,
+(percentile_cont(0.75) within group (order by quantityordered) - percentile_cont(0.25) within group (order by quantityordered))
+as IQR
+from sales_dataset_rfm_prj
+) as IQR_table
+)
+select * from sales_dataset_rfm_prj
+where quantityordered<(select min_value from min_max_table)
+   or quantityordered>(select max_value from min_max_table)
+)
+delete from sales_dataset_rfm_prj
+where quantityordered in(select quantityordered from outliers)   
 
+-- 5.2 Update Outliers Using Z-SCORE = (users-avg)/stddev
+with z_outliers as (
+with cal_table as (
+select quantityordered,
+(select avg(quantityordered) from sales_dataset_rfm_prj) as avg,
+(select stddev(quantityordered) from sales_dataset_rfm_prj) as stddev
+from sales_dataset_rfm_prj
+)
+select quantityordered, 
+(quantityordered-avg)/stddev as z_score
+from cal_table
+where abs((quantityordered-avg)/stddev)>2 -- >2 hay >3 tùy bài toán
+)
+UPDATE sales_dataset_rfm_prj
+SET quantityordered=(select avg(quantityordered) from sales_dataset_rfm_prj) -- thay thành giá trị TB
+where quantityordered in(select quantityordered from z_outliers)
+
+-- Sau khi làm sạch dữ liệu, lưu vào bảng mới tên là SALES_DATASET_RFM_PRJ_CLEAN
+CREATE TABLE SALES_DATASET_RFM_PRJ_CLEAN AS (
+SELECT * FROM
+(
+(with cal_table as (
+select quantityordered,
+(select avg(quantityordered) from sales_dataset_rfm_prj) as avg,
+(select stddev(quantityordered) from sales_dataset_rfm_prj) as stddev
+from sales_dataset_rfm_prj
+)
+select quantityordered, 
+(quantityordered-avg)/stddev as z_score
+from cal_table
+where abs((quantityordered-avg)/stddev)>2 -- >2 hay >3 tùy bài toán
+)
+UPDATE sales_dataset_rfm_prj
+SET quantityordered=(select avg(quantityordered) from sales_dataset_rfm_prj) -- thay thành giá trị TB
+where quantityordered in(select quantityordered from z_outliers)
+) AS cleaned_data
