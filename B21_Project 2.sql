1c2c8e782176f05710359974b10a59b2f6b05813
trangphan06 <160846048+trangphan06@users.noreply.github.com>
2024-04-13 23:02:51 +0700

    Update B21_Project 2.sql

--- B21_Project 2.sql
+++ B21_Project 2.sql
@@ -6,10 +6,17 @@ II. Ad-hoc tasks
 */
 
 select
-format_date('%B %Y', cast(created_at as timestamp)) as month_year,
+format_date('%Y-%m', created_at) as month_year,
 count(distinct user_id) as total_user,
 count(distinct order_id) as total_order
 from bigquery-public-data.thelook_ecommerce.orders
 group by month_year
 order by month_year 
 ;
+
+/* Insight: 
+- Nhìn chung, số lượng order tăng đều theo thời gian, mỗi năm cũng tăng đều, chưa thấy seasonal trend rõ rệt. 
+- Những năm đầu, số lượng total_order và total_user bằng nhau hoặc chênh nhau ít, chứng tỏ mỗi user mua hàng với số lượng lẻ tẻ, nhỏ giọt.
+- Những năm gần đây, số lượng total_user lớn hơn số lượng total_order, chứng tỏ mỗi user bắt đầu mua hàng với số lượng nhiều hơn, đỉnh điểm là trong 4 tháng gần nhất.
+*/
+
