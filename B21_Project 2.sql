092b0aa2e57321d46964d1c0e6526cbbb47f3a39
trangphan06 <160846048+trangphan06@users.noreply.github.com>
2024-04-14 21:38:25 +0700

    Update B21_Project 2.sql

--- B21_Project 2.sql
+++ B21_Project 2.sql
@@ -41,3 +41,123 @@ order by month_year
   Tìm các khách hàng có trẻ tuổi nhất và lớn tuổi nhất theo từng giới tính (Từ 1/2019-4/2022)
   Output: first_name, last_name, gender, age, tag (hiển thị youngest nếu trẻ tuổi nhất, oldest nếu lớn tuổi nhất)
 */
+/* 
+  3. Nhóm khách hàng theo độ tuổi
+  Tìm các khách hàng có trẻ tuổi nhất và lớn tuổi nhất theo từng giới tính (Từ 1/2019-4/2022)
+  Output: first_name, last_name, gender, age, tag (hiển thị youngest nếu trẻ tuổi nhất, oldest nếu lớn tuổi nhất)
+*/
+with youngest_female as 
+(
+select first_name, last_name, gender, age,
+"youngest" as tag
+from bigquery-public-data.thelook_ecommerce.users
+where gender='F'
+and age=(select min(age) 
+         from bigquery-public-data.thelook_ecommerce.users
+         where gender='F')
+),
+youngest_male as 
+(
+select first_name, last_name, gender, age,
+"youngest" as tag
+from bigquery-public-data.thelook_ecommerce.users
+where gender='M'
+and age=(select min(age) 
+         from bigquery-public-data.thelook_ecommerce.users
+         where gender='M')
+),
+oldest_female as 
+(
+select first_name, last_name, gender, age,
+"oldest" as tag
+from bigquery-public-data.thelook_ecommerce.users
+where gender='F'
+and age=(select max(age) 
+         from bigquery-public-data.thelook_ecommerce.users
+         where gender='F')
+),
+oldest_male as 
+(
+select first_name, last_name, gender, age,
+"oldest" as tag
+from bigquery-public-data.thelook_ecommerce.users
+where gender='M'
+and age=(select max(age) 
+         from bigquery-public-data.thelook_ecommerce.users
+         where gender='M')
+),
+tong_hop as 
+(
+select first_name, last_name, gender, age, tag
+from youngest_female
+UNION ALL
+select first_name, last_name, gender, age, tag
+from youngest_male
+UNION ALL
+select first_name, last_name, gender, age, tag
+from oldest_female
+UNION ALL
+select first_name, last_name, gender, age, tag
+from oldest_male
+)
+select gender, tag, count(*)
+from tong_hop
+group by gender, tag
+
+/* Insight:
+- Ở cả 2 gender, trẻ nhất đều là 12 tuổi, lớn nhất đều là 70 tuổi
+- Oldest female có số lượng ít nhất là 784
+- Youngest female có số lượng nhiều nhất là 876
+*/
+
+/* 
+  4.Top 5 sản phẩm mỗi tháng.
+  Thống kê top 5 sản phẩm có lợi nhuận cao nhất từng tháng (xếp hạng cho từng sản phẩm). 
+  Output: month_year (yyyy-mm), product_id, product_name, sales, cost, profit, rank_per_month
+*/
+with cte as 
+(
+select format_date('%Y-%m', a.created_at) as month_year,
+a.product_id as product_id,
+b.name as product_name,
+sum(a.sale_price) as sales,
+sum(b.cost) as cost,
+sum(a.sale_price)-sum(b.cost) as profit
+from bigquery-public-data.thelook_ecommerce.order_items as a
+join bigquery-public-data.thelook_ecommerce.products as b
+on a.product_id=b.id
+group by month_year, a.product_id, b.name
+order by month_year
+),
+rank_table as 
+(
+select *,
+dense_rank() over(partition by month_year order by profit) as rank_per_month
+from cte
+)
+select *
+from rank_table
+where rank_per_month <6
+order by month_year
+;
+
+/* 
+  5.Doanh thu tính đến thời điểm hiện tại trên mỗi danh mục
+  Thống kê tổng doanh thu theo ngày của từng danh mục sản phẩm (category) trong 3 tháng qua (giả sử ngày hiện tại là 15/4/2022) 
+  Output: dates (yyyy-mm-dd), product_categories, revenue
+*/
+with cte as
+(
+select format_date('%Y-%m-%d', a.created_at) as dates,
+b.category as product_categories,
+sum(a.sale_price) as revenue
+from bigquery-public-data.thelook_ecommerce.order_items as a
+join bigquery-public-data.thelook_ecommerce.products as b
+on a.product_id=b.id
+group by dates, b.category
+)
+select * from cte
+where PARSE_DATE('%Y-%m-%d',dates) >= date_sub('2022-04-15', interval 3 month)
+and dates<='2022-04-15'
+order by dates
+;
