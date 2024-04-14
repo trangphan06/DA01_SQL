497362bb1c000e860b113e289008ec49e88a1268
trangphan06 <160846048+trangphan06@users.noreply.github.com>
2024-04-14 16:56:52 +0700

    Update B21_Project 2.sql

--- B21_Project 2.sql
+++ B21_Project 2.sql
@@ -1,4 +1,4 @@
-II. Ad-hoc tasks
+/* II. Ad-hoc tasks
   1. Số lượng đơn hàng và số lượng khách hàng mỗi tháng
   Thống kê tổng số lượng người mua và số lượng đơn hàng đã hoàn thành mỗi tháng (Từ 1/2019-4/2022)
   Output: month_year (yyyy-mm) , total_user, total_order
@@ -20,3 +20,24 @@ order by month_year
 - Những năm gần đây, số lượng total_user lớn hơn số lượng total_order, chứng tỏ mỗi user bắt đầu mua hàng với số lượng nhiều hơn, đỉnh điểm là trong 4 tháng gần nhất.
 */
 
+/* 
+  2. Giá trị đơn hàng trung bình (AOV) và số lượng khách hàng mỗi tháng
+  Thống kê giá trị đơn hàng trung bình và tổng số người dùng khác nhau mỗi tháng (Từ 1/2019-4/2022)
+  Output: month_year (yyyy-mm), distinct_users, average_order_value
+*/
+
+select format_date('%Y-%m', b.created_at) as month_year,
+count(distinct b.user_id) as distinct_users,
+round(avg(a.sale_price*b.num_of_item),2) as average_order_value
+from bigquery-public-data.thelook_ecommerce.order_items as a
+join bigquery-public-data.thelook_ecommerce.orders as b
+on b.order_id=a.order_id
+where format_date('%Y-%m', b.created_at) < '2022-05'
+group by month_year
+order by month_year 
+
+/* 
+  3. Nhóm khách hàng theo độ tuổi
+  Tìm các khách hàng có trẻ tuổi nhất và lớn tuổi nhất theo từng giới tính (Từ 1/2019-4/2022)
+  Output: first_name, last_name, gender, age, tag (hiển thị youngest nếu trẻ tuổi nhất, oldest nếu lớn tuổi nhất)
+*/
