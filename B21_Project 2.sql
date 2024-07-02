6d02c640c16763c3ebc9b402aafb06ec8490ec6b
trangphan06 <160846048+trangphan06@users.noreply.github.com>
2024-07-02 18:31:00 +0700

    Update B21_Project 2.sql

--- B21_Project 2.sql
+++ B21_Project 2.sql
@@ -1,4 +1,5 @@
-/* II. Ad-hoc tasks
+Ecommerce Dataset: Exploratory Data Analysis (EDA) and Cohort Analysis in SQL
+/* Ad-hoc tasks
   1. Số lượng đơn hàng và số lượng khách hàng mỗi tháng
   Thống kê tổng số lượng người mua và số lượng đơn hàng đã hoàn thành mỗi tháng (Từ 1/2019-4/2022)
   Output: month_year (yyyy-mm) , total_user, total_order
